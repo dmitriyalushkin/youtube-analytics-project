eb738cfff2b68ca89ff862560ea00835c0ba54d5
dmitriyalushkin <pantific37@mail.ru>
2023-10-01 15:19:26 +0300

    Initial commit6

--- src/video.py
+++ src/video.py
@@ -8,13 +8,18 @@ class Video:
     api_key = os.getenv('API_KEY')
     def __init__(self, video_id):
         self.video_id = video_id
+        try:
+            video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=self.video_id).execute()
 
-        video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=self.video_id).execute()
-
-        self.title = video_response['items'][0]['snippet']['title']
-        self.url = f'https://www.youtube.com/watch?v={self.video_id}'
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+            self.title = video_response['items'][0]['snippet']['title']
+            self.url = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        except:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
 
     def __str__(self):
@@ -34,6 +39,13 @@ class PLVideo(Video):
 
     def __str__(self):
         return f'{self.title}'
+
+
+dev = Video('AWX4JnAnjBE')
+print(dev)
+dev1 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
+print(dev1)
+
 
 
 
